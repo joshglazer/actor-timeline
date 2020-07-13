6494ef0ab57aec5fde45727b13ebbf1a62f1df43
Josh Glazer <joshglazer@gmail.com>
2020-07-13 17:34:14 -0400

    built ratings chart for person view

--- src/app/features/view-person/view-person.component.ts
+++ src/app/features/view-person/view-person.component.ts
@@ -13,6 +13,15 @@ export class ViewPersonComponent implements OnInit {
 
   public person: TmdbPerson;
   public movies: TmdbMovieCredits;
+  public moviesChartData: {
+    x: Date;
+    y: number;
+  }[];
+
+  public primaryXAxis: Object;
+  public chartData: Object[];
+  public title: string;
+  public primaryYAxis: Object;
 
   constructor(private route: ActivatedRoute, private tmdbApiService: TmdbApiService) {}
 
@@ -22,6 +31,16 @@ export class ViewPersonComponent implements OnInit {
       this.loadPerson();
       this.loadMovieCredits();
     });
+
+    this.primaryXAxis = {
+      valueType: 'DateTime',
+      title: 'Movie Release Dates',
+      labelFormat: 'yMMM',
+    };
+    this.primaryYAxis = {
+      title: 'Ratings',
+    };
+    this.title = 'Ratings over Time';
   }
 
   loadPerson(): void {
@@ -33,6 +52,22 @@ export class ViewPersonComponent implements OnInit {
   loadMovieCredits(): void {
     this.tmdbApiService.searchForMoviesByPersonID(this.personID).subscribe((movies: TmdbMovieCredits) => {
       this.movies = movies;
+      const moviesChartDataUnsorted = [];
+      for (const movie of movies.cast) {
+        if (movie.release_date !== '' && movie.release_date !== undefined && movie.vote_count > 0) {
+          const releaseDate = movie.release_date.split('-');
+          const releaseDatePersed = new Date(
+            parseInt(releaseDate[0], 10),
+            parseInt(releaseDate[1], 10),
+            parseInt(releaseDate[2], 10)
+          );
+          moviesChartDataUnsorted.push({
+            x: releaseDatePersed,
+            y: movie.vote_average,
+          });
+        }
+      }
+      this.moviesChartData = moviesChartDataUnsorted.sort((a, b) => (a.x > b.x ? 1 : -1));
     });
   }
 }
--- src/app/features/view-person/view-person.module.ts
+++ src/app/features/view-person/view-person.module.ts
@@ -4,12 +4,17 @@ import { CommonModule } from '@angular/common';
 import { ViewPersonRoutingModule } from './view-person-routing.module';
 import { ViewPersonComponent } from './view-person.component';
 
+import { ChartModule } from '@syncfusion/ej2-angular-charts';
+import {
+  DateTimeService,
+  LineSeriesService,
+  DateTimeCategoryService,
+  StripLineService,
+} from '@syncfusion/ej2-angular-charts';
 
 @NgModule({
   declarations: [ViewPersonComponent],
-  imports: [
-    CommonModule,
-    ViewPersonRoutingModule
-  ]
+  imports: [CommonModule, ViewPersonRoutingModule, ChartModule],
+  providers: [DateTimeService, LineSeriesService, DateTimeCategoryService, StripLineService],
 })
-export class ViewPersonModule { }
+export class ViewPersonModule {}
